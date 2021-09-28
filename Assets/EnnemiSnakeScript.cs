52d176910bc4bb4d518d2ebf819e01bff78a88d6
Mathieu Felton <mfelton170@gmail.com>
2021-09-28 19:07:57 -0400

    ajout dun ennemi de base

---
+++ Assets/EnnemiSnakeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiSnakeScript : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform[] waypoints;
+    private Transform targetWaypoint;
+    private int destinationPoint = 0;
+    [SerializeField] private SpriteRenderer sprite;
+
+    void Start()
+    {
+        targetWaypoint = waypoints[0];
+    }
+    
+    void Update()
+    {
+        Vector3 direction = targetWaypoint.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.3f)
+        {
+            destinationPoint = (destinationPoint + 1) % waypoints.Length;
+            targetWaypoint = waypoints[destinationPoint];
+            sprite.flipX = !sprite.flipX;
+        }
+    }
+}
